c4c04cffb45449820ea123a1c006a92b8b96d8e0
Shontal Biton <shontal1005@gmail.com>
2025-09-16 21:12:09 +0300

    Radio driver finished and tested
    Raido driver with variable length support.
    Signed-off-by: Shontal Biton <shontal1005@gmail.com>

--- drivers/radio/efr32_radio_gecko.c
+++ drivers/radio/efr32_radio_gecko.c
@@ -1,8 +1,8 @@
 #define DT_DRV_COMPAT efr32_radio_gecko
 
 #include <stdio.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/irq.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/sys/byteorder.h>
 
 #include "efr32_radio_gecko.h"
@@ -13,28 +13,33 @@ LOG_MODULE_REGISTER(driver);
 
 struct radio_data* radio_driver_data = NULL;
 const struct radio_api* radio_driver_api = NULL;
-static __ALIGNED(RAIL_FIFO_ALIGNMENT) uint8_t rx_fifo[CONFIG_RADIO_EFR32_RX_BUFFER_SIZE];
 
 #define VARIABLE_LENGTH_LENGTH_FIELD (2)
 
-/**
- * From silabs rail efr32 documentation: https://docs.silabs.com/rail/latest/rail-api/efr32-main
- * The chosen buffer size limits the maximum size of receive packets in packet mode and determines the size of the receive
- * FIFO in FIFO mode. Because each receive packet has several bytes of overhead,
- * you can only receive up to one (buffer size - overhead) byte packet without switching to FIFO mode.
- * In FIFO mode, you must read out packet data as you approach this limit and store it off to construct the full packet later.
- * This overhead is currently 8 bytes on all EFR32 Series-2 platforms. Note that this overhead may increase or decrease
- * in future releases as the functionality is changed though large jumps are not expected in either direction.
- *
- * The Rail overhead is added to each packet added to RX FIFO
- */
-#define RADIO_EFR32_PACKET_STORAGE_OVERHEAD (8)
+#ifdef CONFIG_RADIO_EFR32
+	/**
+	 * From silabs rail efr32 documentation: https://docs.silabs.com/rail/latest/rail-api/efr32-main
+	 * The chosen buffer size limits the maximum size of receive packets in packet mode and determines the size of the receive
+	 * FIFO in FIFO mode. Because each receive packet has several bytes of overhead,
+	 * you can only receive up to one (buffer size - overhead) byte packet without switching to FIFO mode.
+	 * In FIFO mode, you must read out packet data as you approach this limit and store it off to construct the full packet later.
+	 * This overhead is currently 8 bytes on all EFR32 Series-2 platforms. Note that this overhead may increase or decrease
+	 * in future releases as the functionality is changed though large jumps are not expected in either direction.
+	 *
+	 * The RAIL overhead is added to each packet in RX FIFO
+	 *
+	 * Payload size most be smaller than FIFO_SIZE - EFR32_PACKET_STORAGE_OVERHEAD(8 bytes) - SIZE_FIELD_LENGTH(2 bytes)
+	 */
+	#define RADIO_EFR32_PACKET_STORAGE_OVERHEAD (8)
+#else
+	#define RADIO_EFR32_PACKET_STORAGE_OVERHEAD (0)
+#endif
 
 /**
  * This is the size of the actual data from the user. In the
  * driver, the payload size (2 bytes) and rail overhead (8 bytes) are added to the payload.
  */
-#define RADIO_EFR32_MAX_PACKET_LENGTH (CONFIG_RADIO_EFR32_RX_BUFFER_SIZE - RADIO_EFR32_PACKET_STORAGE_OVERHEAD - VARIABLE_LENGTH_LENGTH_FIELD)
+#define RADIO_EFR32_MAX_PACKET_LENGTH (CONFIG_RADIO_EFR32_TX_BUFFER_SIZE - RADIO_EFR32_PACKET_STORAGE_OVERHEAD - VARIABLE_LENGTH_LENGTH_FIELD)
 
 typedef enum communication_status {
 
@@ -52,12 +57,31 @@ typedef enum communication_status {
 
 } communication_status_t;
 
+/**
+ * Callback function that will be called after RAIL_ConfigChannels is done.
+ *
+ * @param[in] railHandle A RAIL instance handle.
+ * @param[in] entry A pointer to channel config entry.
+ *
+ */
 static void rail_on_channel_config(RAIL_Handle_t rail_handle,
 				   const RAIL_ChannelConfigEntry_t *entry)
 {
 	sl_rail_util_pa_on_channel_config_change(rail_handle, entry);
 }
 
+/**
+ * Radio initialization function.
+ * This function init all the user configurable configurations
+ *
+ * @param[in] radio_dev The radio device object.
+ * @param[in] configuration PHY configuration number.
+ * @param[in] channel The radio channel to work with.
+ * @param[in] callback Callback function to be called after the initialization is over.
+ *
+ * @return Status code indicates error.
+ *
+ */
 int efr32_radio_init(const struct device* radio_dev, uint32_t configuration, uint16_t channel, radio_event_callback callback)
 {
 	RAIL_Status_t status;
@@ -77,6 +101,12 @@ int efr32_radio_init(const struct device* radio_dev, uint32_t configuration, uin
 	return RADIO_SUCCESS;
 }
 
+/**
+ * Send continuous wave in the channel configured in the driver
+ *
+ * @return Status code indicates error.
+ *
+ */
 int efr32_radio_start_cw()
 {
 	RAIL_Status_t status;
@@ -90,6 +120,12 @@ int efr32_radio_start_cw()
 	return RADIO_SUCCESS;
 }
 
+/**
+ * Stop the continuous wave send.
+ *
+ * @return Status code indicates error.
+ *
+ */
 int efr32_radio_stop_cw()
 {
 	RAIL_Status_t status;
@@ -103,16 +139,25 @@ int efr32_radio_stop_cw()
 	return RADIO_SUCCESS;
 }
 
+/**
+ * In RAIL variable length mode, we can create packets in different
+ * sizes. Variable length packet format requires the first 2 bytes to be the payload length
+ * and after that the data is inserted.
+ * The function get the payload and size, and create the packet buffer in the correct variable
+ * length format.
+ *
+ * @param[in,out] payload_to_send A buffer for the variable length packet buffer.
+ * @param[in] payload The data to send.
+ * @param[in] len Payload size.
+ *
+ *
+ * @return Status code indicates error.
+ *
+ */
 int create_radio_packet(uint8_t* payload_to_send, const uint8_t* payload, uint16_t len)
 {
 	*(uint16_t*)payload_to_send = sys_cpu_to_le16(len);
-
-	if (len > RADIO_EFR32_MAX_PACKET_LENGTH)
-	{
-		return RADIO_TX_CREATE_PACKET_FAILED;
-	}
-
-	void* ret = memcpy(payload_to_send + 2, payload, len);
+	void* ret = memcpy(payload_to_send + VARIABLE_LENGTH_LENGTH_FIELD, payload, len);
 	if (ret == NULL)
 	{
 		return RADIO_TX_CREATE_PACKET_FAILED;
@@ -121,10 +166,28 @@ int create_radio_packet(uint8_t* payload_to_send, const uint8_t* payload, uint16
 	return RADIO_SUCCESS;
 }
 
+/**
+ * Transmits the given data through the given channel.
+ *
+ * @param[in] channel The channel to use.
+ * @param[in] payload The data to send.
+ * @param[in] len Payload size.
+ * @param[in] clear_fifo Whether to clear the transmit FIFO before inserting new data.
+ * (NOT recommended if previous data may still exist)
+ *
+ * @return Status code indicates error.
+ *
+ */
 int efr32_radio_send(uint16_t channel, const uint8_t* payload, int len, bool clear_fifo)
 {
 	RAIL_Status_t status;
 	int ret;
+
+	if (len > RADIO_EFR32_MAX_PACKET_LENGTH || len <= 0)
+	{
+		LOG_ERR("radio_send() invalid payload length %d", len);
+		return RADIO_TX_CREATE_PACKET_FAILED;
+	}
 
 	ret = create_radio_packet(radio_driver_data->payload_to_send, payload, len);
 	if (ret != RADIO_SUCCESS)
@@ -148,6 +211,15 @@ int efr32_radio_send(uint16_t channel, const uint8_t* payload, int len, bool cle
 	return RADIO_SUCCESS;
 }
 
+/**
+ * Start the receiver on a specific channel.
+ * This is a non-blocking function.
+ *
+ * @param[in] channel The channel to use.
+ *
+ * @return Status code indicates error.
+ *
+ */
 int efr32_radio_start_rx_listening(uint16_t channel)
 {
 	RAIL_Status_t status = RAIL_StartRx(radio_driver_data->rail_handle, channel, NULL);
@@ -159,6 +231,14 @@ int efr32_radio_start_rx_listening(uint16_t channel)
 	return RADIO_SUCCESS;
 }
 
+/**
+ * Get the RX packet in case RX event occured.
+ *
+ * @param[in] rail_handle A RAIL instance handle.
+ *
+ * @return Status code indicates error.
+ *
+ */
 int rx_packets(RAIL_Handle_t rail_handle)
 {
 	RAIL_RxPacketHandle_t handle;
@@ -185,6 +265,16 @@ int rx_packets(RAIL_Handle_t rail_handle)
 	return RX_SUCCESS;
 }
 
+/**
+ * A callback that will be executed when a RAIL event occurs.
+ * This function handles various radio states and calls the appropriate function on each event.
+ *
+ * @param[in] rail_handle A RAIL instance handle.
+ * @param[in] events The RAIL event that occured.
+ *
+ * @return Status code indicates error.
+ *
+ */
 void rail_on_event(RAIL_Handle_t rail_handler, RAIL_Events_t events)
 {
 	RAIL_Status_t status;
@@ -202,8 +292,8 @@ void rail_on_event(RAIL_Handle_t rail_handler, RAIL_Events_t events)
 			communication_status = rx_packets(rail_handler);
 			if (communication_status == RX_SUCCESS) {
 				radio_driver_data->event.event = RX_DONE;
-				radio_driver_data->event.data_size = (uint16_t)radio_driver_data->received_packet_buffer[0] | ((uint16_t)radio_driver_data->received_packet_buffer[1] << 8);
 				radio_driver_data->event.data = radio_driver_data->received_packet_buffer + VARIABLE_LENGTH_LENGTH_FIELD;
+				radio_driver_data->event.data_size = sys_le16_to_cpu(*(uint16_t*)radio_driver_data->received_packet_buffer);
 				radio_driver_data->callback(&radio_driver_data->event);
 			}
 	} else if ((events & RAIL_EVENTS_RX_COMPLETION) && (communication_status != RX_SUCCESS)) {
@@ -231,6 +321,10 @@ void rail_on_event(RAIL_Handle_t rail_handler, RAIL_Events_t events)
 	}
 }
 
+/**
+ * Configure all the needed interrupts for the
+ * radio to work.
+ */
 static void rail_isr_installer(void)
 {
 #if defined(CONFIG_SOC_SERIES_EFR32MG24) || defined(CONFIG_SOC_SERIES_EFR32ZG28)
@@ -247,11 +341,27 @@ static void rail_isr_installer(void)
 	IRQ_CONNECT(AGC_IRQn, 0, AGC_IRQHandler, NULL, 0);
 }
 
+/**
+ * Callback function to be called when the RF initialization
+ * is over.
+ *
+ * @param[in] rail_handle A RAIL instance handle.
+ *
+ * */
 static void rail_on_rf_ready(RAIL_Handle_t rail_handle)
 {
 	LOG_INF("radio is ready %p", rail_handle);
 }
 
+/**
+ * Radio initialization function.
+ * This function init all the default radio configurations.
+ *
+ * @param[in] radio_dev The radio device object.
+ *
+ * @return 0 when the funcion is over.
+ *
+ */
 static int efr32_radio_initialization(const struct device* dev)
 {
 	struct radio_data* data = (struct radio_data*)dev->data;
@@ -275,6 +385,7 @@ static int efr32_radio_initialization(const struct device* dev)
 
 	RAIL_Status_t status;
 	int ret;
+	uint16_t RX_buffer_size = CONFIG_RADIO_EFR32_RX_BUFFER_SIZE;
 
 	data->rail_handle = RAIL_Init(&rail_config, &rail_on_rf_ready);
 	if (!data->rail_handle) {
@@ -310,11 +421,11 @@ static int efr32_radio_initialization(const struct device* dev)
 	if (ret != CONFIG_RADIO_EFR32_TX_BUFFER_SIZE) {
 		LOG_ERR("RAIL_SetTxFifo(): %d != %d", ret, CONFIG_RADIO_EFR32_TX_BUFFER_SIZE);
 	}
-	uint16_t rx_buffer_size = CONFIG_RADIO_EFR32_RX_BUFFER_SIZE;
-	status = RAIL_SetRxFifo(data->rail_handle, &(rx_fifo)[0], &rx_buffer_size);
-	if (status || rx_buffer_size != CONFIG_RADIO_EFR32_RX_BUFFER_SIZE) {
-		LOG_ERR("RAIL_SetRxFifo(): status: %d, sizes: %d != %d", status, rx_buffer_size, CONFIG_RADIO_EFR32_RX_BUFFER_SIZE);
+	status = RAIL_SetRxFifo(data->rail_handle, &(data->RX_buffer)[0], &RX_buffer_size);
+	if (status || RX_buffer_size != CONFIG_RADIO_EFR32_RX_BUFFER_SIZE) {
+		LOG_ERR("RAIL_SetRxFifo(): status: %d, sizes: %d != %d", status, RX_buffer_size, CONFIG_RADIO_EFR32_RX_BUFFER_SIZE);
 	}
+
 	return 0;
 }
 
@@ -330,7 +441,7 @@ static const struct radio_api efr32_radio_api = {
     static const struct radio_conf radio_conf_##inst = { 		   \
     };															   \
 	                                                               \
-	static struct radio_data radio_##inst##_data = {0};                   \
+	static struct radio_data radio_##inst##_data = {0};            \
                                                                    \
     DEVICE_DT_INST_DEFINE(inst,                                    \
                   efr32_radio_initialization,                      \
--- drivers/radio/efr32_radio_gecko.h
+++ drivers/radio/efr32_radio_gecko.h
@@ -3,13 +3,30 @@
 #include <zephyr/drivers/radio_gecko.h>
 
 struct radio_data {
+	/* RAIL handle instance */
 	RAIL_Handle_t rail_handle;
+
+	/* Event for the user to use in callback*/
 	radio_event event;
+
+	/* The user callback to be called on events */
 	radio_event_callback callback;
+
+	/* The radio configuration to use */
 	uint32_t configuration;
+
+	/* The radio channel to use */
 	uint16_t channel;
+
+	/* Buffer to hold the variable length packet (packet and size) */
 	uint8_t payload_to_send[CONFIG_RADIO_EFR32_TX_BUFFER_SIZE];
+
+	/* Buffer to hold the received packet */
 	uint8_t received_packet_buffer[CONFIG_RADIO_EFR32_RX_BUFFER_SIZE];
+
+	/* Buffer given for RAIL to use for transmiting */
 	uint8_t TX_buffer[CONFIG_RADIO_EFR32_TX_BUFFER_SIZE] __aligned(4);
-	// uint8_t RX_buffer[CONFIG_RADIO_EFR32_RX_BUFFER_SIZE] __aligned(4);
+
+	/* Buffers given for RAIL to use for receiving */
+	uint8_t RX_buffer[CONFIG_RADIO_EFR32_RX_BUFFER_SIZE] __aligned(4);
 } __packed;
